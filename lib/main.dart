6d5de78c220156ce9c9f36760a5d90e185671188
Christian Vogel <chris.vogel@me.com>
2018-09-13 15:53:02 +0200

    cleaned up to represent the original starter project. test code moved to test branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tester/app-model.dart';
-import 'package:tester/pages/animations/animations.dart';
 import 'package:tester/pages/home/home.dart';
-import 'package:tester/pages/test/test.dart';
-import 'package:tester/test-model.dart';
 
 void main() {
   runApp(TestApp());
@@ -11,10 +8,7 @@ void main() {
 
 class TestApp extends StatelessWidget {
   final model = AppModel();
-  final testModel = TestModel();
 
   @override
   build(context) => HomePage(model: model);
-  // build(context) => TestPage(model: testModel);
-  // build(context) => AnimationsPage(model: model);
 }
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -3,40 +3,36 @@ import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:flutter/cupertino.dart';
 // ignore: unused_import
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-// ignore: unused_import
 import 'package:scoped_model/scoped_model.dart';
 // ignore: unused_import
 import 'package:tester/app-model.dart';
-// ignore: unused_import
-import 'package:tester/util/controller_widget.dart';
 // ignore: unused_import
 import 'package:tester/util/reactive-model.dart';
 
 // ignore: non_constant_identifier_names
 MaterialApp HomePage({ model }) {
-  return MaterialApp( // project://lib/pages/home/home.pug#8,2
+  return MaterialApp( // project://lib/pages/home/home.pug#6,2
     title: 'Flutter Demo!',
-    home: Scaffold( // project://lib/pages/home/home.pug#9,3
-      appBar: AppBar( // project://lib/pages/home/home.pug#10,4
-        title: Container( // project://lib/pages/home/home.pug#11,5
+    home: Scaffold( // project://lib/pages/home/home.pug#7,3
+      appBar: AppBar( // project://lib/pages/home/home.pug#8,4
+        title: Container( // project://lib/pages/home/home.pug#9,5
           child: Text( 
             'Welcome'
           )
         )
       ),
-      body: Center( // project://lib/pages/home/home.pug#12,4
-        child: ReactiveModel( // project://lib/pages/home/home.pug#13,5
+      body: Center( // project://lib/pages/home/home.pug#10,4
+        child: ReactiveModel( // project://lib/pages/home/home.pug#11,5
           model: model,
           builder: (context, model) {
-            return Container( // project://lib/pages/home/home.pug#14,6
+            return Container( // project://lib/pages/home/home.pug#12,6
               child: Column( 
                 children: __flatten([
                   Text( 
                     'You have pushed:'
                   ),
                   DefaultTextStyle( 
-                    child: Container( // project://lib/pages/home/home.pug#15,7
+                    child: Container( // project://lib/pages/home/home.pug#13,7
                       child: Text( 
                         '${model.counter} times'
                       ),
@@ -54,10 +50,10 @@ MaterialApp HomePage({ model }) {
           }
         )
       ),
-      floatingActionButton: FloatingActionButton( // project://lib/pages/home/home.pug#17,4
+      floatingActionButton: FloatingActionButton( // project://lib/pages/home/home.pug#15,4
         tooltip: 'Increment',
         onPressed: () { model.incrementCounter(); },
-        child: Icon( // project://lib/pages/home/home.pug#21,5
+        child: Icon( // project://lib/pages/home/home.pug#19,5
           Icons.add
         )
       )
